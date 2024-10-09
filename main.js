9bd385275d30275113b53f49a90d37e5b9085426
crecat <112257840+Ben-Kei-create@users.noreply.github.com>
2024-10-09 20:56:52 +0900

    study javaScript

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+JavaScriptにおける数値表現
+*/
+
+console.log(100);
+console.log(-1);
+console.log(1.5);
+
+//大きな数値の表現
+sonsole.log(1.2e4);
+
+'use strict';
+
+console.log(10 + 3); // 13
+console.log(10 - 3); // 7
+console.log(10 * 3); // 30
+console.log(10 ** 3); // 1000
+console.log(10 / 3); // 3.3333....
+console.log(10 % 3); // 1
+
+console.log(10 + 2 * 3); // 16
+console.log((10 + 2) * 3); // 36
